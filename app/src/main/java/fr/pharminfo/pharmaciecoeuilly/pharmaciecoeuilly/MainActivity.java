29fb647d305ec45b68611ed5e0baad87831cec14
Nxuan <nxtxuan@hotmail.com>
2017-04-10 23:00:55 +0200

    First commit on GitHub
    First commit on Github

---
+++ app/src/main/java/fr/pharminfo/pharmaciecoeuilly/pharmaciecoeuilly/MainActivity.java
@@ -0,0 +1,110 @@
+package fr.pharminfo.pharmaciecoeuilly.pharmaciecoeuilly;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    File photoFile;
+    String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /* openLocation
+    /  Opens location of the shop in Maps application
+     */
+    public void openLocation(View view) {
+        String myAddress = "Pharmacie+de+Coeuilly+94500+Champigny+sur+marne+France";
+        String uriMap = "geo:0,0?q=" + myAddress;
+        Intent locationIntent = new Intent(Intent.ACTION_VIEW);
+        locationIntent.setData(Uri.parse(uriMap));
+        if (locationIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(locationIntent);
+        }
+    }
+    /* openPhone
+    /  Calls the shop in call application
+     */
+    public void openPhone(View view) {
+        String phoneNumber="+33148813836";
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    /* takePic
+    /  Gets a picture of the prescription
+     */
+    public void takePic(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "fr.pharminfo.pharmaciecoeuilly.pharmaciecoeuilly.MainActivity",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String[] addresses = {"nxuann@gmail.com"};
+        String subject="Please prepare this prescription";
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_STREAM, photoFile);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
